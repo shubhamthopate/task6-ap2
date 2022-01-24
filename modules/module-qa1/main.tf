f4dd68b2d8c708f9063db8542bdedd5eea4c9abc
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-24 10:08:22 +0530

    sdfdfv

--- modules/module-qa1/main.tf
+++ modules/module-qa1/main.tf
@@ -6,23 +6,23 @@ resource "azurerm_resource_group" "example2" {
 
 resource "azurerm_log_analytics_workspace" "example2" {
   name                = var.workspacenameqa
-  location            = azurerm_resource_group.example1.location
-  resource_group_name = azurerm_resource_group.example1.name
+  location            = azurerm_resource_group.example2.location
+  resource_group_name = azurerm_resource_group.example2.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
 resource "azurerm_application_insights" "example2" {
   name                = var.insightnameqa
-  location            = azurerm_resource_group.example1.location
-  resource_group_name = azurerm_resource_group.example1.name
-  workspace_id        = azurerm_log_analytics_workspace.example1.id
+  location            = azurerm_resource_group.example2.location
+  resource_group_name = azurerm_resource_group.example2.name
+  workspace_id        = azurerm_log_analytics_workspace.example2.id
   application_type    = "web"
 }
 
 resource "azurerm_application_insights" "example3" {
   name                = var.insight2qa
-  location            = azurerm_resource_group.example1.location
-  resource_group_name = azurerm_resource_group.example1.name
+  location            = azurerm_resource_group.example2.location
+  resource_group_name = azurerm_resource_group.example2.name
   application_type    = "web"
 }
